402afa4808c9e090bcecd13153dcab18e87acfe8
iamoziroz <nickpap556@gmail.com>
2019-12-30 16:46:13 +0200

    removed unrelated comments

--- mailduck.ino
+++ mailduck.ino
@@ -141,9 +141,7 @@ void setup()
 void loop(){
   blinkLed();
     if (Serial1.available() > 0) {
-    // read incoming serial data:
     String inChar = Serial1.readString();
-    // Type the next ASCII value from what you received:
     Keyboard.print(inChar);
   }
 }
