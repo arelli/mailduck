6866ae4c67b35f5cb18aec8f35870b414dddbd6e
iamoziroz <nickpap556@gmail.com>
2019-12-31 14:56:51 +0200

    added help output in main page
    help output about modifier key codes and command usage

--- wifi_controller.ino
+++ wifi_controller.ino
@@ -38,11 +38,22 @@ String handleGenericArgs() { //Handler from https://techtutorialsx.com/2016/10/2
 
 void setup(void){
   //the HTML of the web page
-  page = "<h1>WifiDuck Remote Control</h1></br><form>Command to Send:<br><input type=\"text\" name=\"command\"><br></form>";
+  page = "<h1>WifiDuck Remote Control</h1>";
+  page += "Usage:";
+  page += "</br><font size=\"1\">cmd(space)(text): Prints the text and then hits enter(return key)</br>";
+  page += "wrt(space)(character): Writes a signle character</br>";
+  page += "prs(space)(ASCII_or_Modifier_Key_Code): Presses the provided key";
+  page += "rls(nothing): Releases all the pressed keys</br>";
+  page += "dbg(anything): used for debugging.The ducky ignores them(available at leonardo usb serial).</font></br>";
+  page += "</br><form>Command to Send:<br><input type=\"text\" name=\"command\"><br></form>";
+  page += "Modifier Key Codes:</br>";  // TODO: add those in a seprate /help directory and add a key to acces it from "/"
+  page += "<font size=\"1\">LEFT_CTRL->128,LEFT_SHIFT->129,LEFT_ALT->130,LEFT_GUI->131,RIGHT_CTRL->132</br>";
+  page += "RIGHT_SHIFT->133,RIGHT_ALT->134,RIGHT_GUI->135,UP_ARROW->218,DOWN_ARROW->217</br>";
+  page += "LEFT_ARROW->216,RIGHT_ARROW->215,BACKSPACE->178,TAB->179,RETURN->176,ESC->177</br>";
+  page += "INSERT->209,DELETE->212,PAGE_UP->211,PAGE_DOWN->214,HOME->210,END->213</br>";
+  page += "CAPS_LOCK->193,F1->194,F2->195,F3->196,F4->197,F5->198,F6->199,F7->200,F8->201</br>";
+  page += "F9->202,F10->203,F11->204,F12->205</font></br>";
   //make the LED pin output and initially turned off
-  pinMode(LEDPin, OUTPUT);
-  digitalWrite(LEDPin, LOW);
-   
   delay(1000);
   Serial.begin(115200);
   WiFi.softAP(ssid1,password1); //begin WiFi connection https://arduino-esp8266.readthedocs.io/en/latest/esp8266wifi/soft-access-point-class.html
@@ -51,8 +62,8 @@ void setup(void){
  
   // Wait for connection
   while (WiFi.softAPgetStationNum() == 0) {
-    delay(500);
-    //Serial.print("wifi_not_connected, ");
+    delay(1000);
+    Serial.print("dbg-no device connected to the AP.");
     //Serial.println(millis());
     //Serial.println(WiFi.status());
     //Serial.println(WiFi.softAPgetStationNum());  //using the SoftAP class
